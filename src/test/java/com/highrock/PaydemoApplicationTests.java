3523c04eb61217155cf2dcc88c2b53d5ef64ea09
zjw1909966713 <zjw1909966713@163.com>
2020-06-08 18:45:00 +0800

    zhifu

---
+++ src/test/java/com/highrock/PaydemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.highrock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaydemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
